59f8d2e604f85e56b505bfb3796e4efa30284bcd
tamara97.b <tamarabassam1997@icloud.com>
2021-06-20 19:46:29 +0300

    tttt

--- cars.js
+++ cars.js
@@ -1,7 +1,18 @@
-let name =prompt("enter your name")
-console.log("name    "+name)
-let country =prompt("enter your country")
-console.log ("country  "+country)
+let name =prompt("enter your name");
+console.log("name    "+name);
+alert("name    "+name);
 
-let car =prompt("do u love cars?")
-let b =prompt("wich type do you love?")
+let country =prompt("enter your country");
+console.log ("country  "+country);
+alert("country  "+country);
+
+
+
+let car =prompt("do u love cars?");
+console.log=("car"+car);
+alert("car"+car);
+
+
+let b =prompt("wich type do you love?");
+console.log("type"+b);
+alert("type"+b);
